1ec9af73dd3ff98bee8408a36120502359862c31
984974895@qq.com <984974895@qq.com>
2019-02-05 18:17:03 +0800

    finish

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ chrome_options = Options()
 class Transfom():
 
     def main(self):
-        
+        # 获取当前目录和文件
         for root, dirs, files in os.walk("./",topdown=True):  
             
             break
-        # a = os.walk("./",topdown=True)[0]
-        # print(a)
-        # print(dirs)  
+      
 
         url_list = []
         dir_list = []
+
         for dir in dirs:
+            # 拼接地址
             url = os.path.join(dir, "report.html")
             if os.path.exists(url):
                 url_list.append('./'+url)
@@ -31,39 +31,46 @@ class Transfom():
                 # url_list = append(url_list,url)
 
         flag = 0
+
         for url in url_list:
+
             with open(url, 'r', encoding='utf-8') as f:
                 html = etree.HTML(f.read())
                 html_data = html.xpath('//div[@id="nav_container"]//dt//a/@href')  
                 # //*[@id="main_nav_list"]/dt//a[@target="detail_report_content"]
                 html_text = html.xpath('//*[@id="main_nav_list"]/dt//a/text()')
                 for i in range(len(html_data)):
-                    pass
+                    
                     self.save_one(url+"/../"+html_data[i],dir_list[flag],html_text[i])
             flag += 1   
+            # break
     def save_one(self,url,dir,filename):
-        pass
-        #chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--disable-infobars')
-        chrome_options.add_argument('--start-maximized')
+        # 开启无界面模式
+        chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
+        # chrome_options.add_argument('--disable-infobars')
+        # chrome_options.add_argument('--start-maximized')
         
         # chrome_options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
         driver=webdriver.Chrome(chrome_options=chrome_options)
        
-        driver.get("file:///home/dly667/python/CutChartByWebdriver/"+url)
-        #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL, '-')
-        driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL, '-')
-        driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL, '-')
-        driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL, '-')
+        driver.get("file:///Users/dly667/dev/CutChartByWebdriver/"+url)
+        # 网页缩放
+        driver.execute_script('document.getElementsByTagName("body")[0].style.zoom=0.38')
+        # 隐藏X和Y轴的滚动条
+        driver.execute_script("document.body.parentNode.style.overflowY = 'hidden';")
+        driver.execute_script("document.body.parentNode.style.overflowX = 'hidden';")
+
+        # 全屏截图
         util.fullpage_screenshot(driver, filename+".png",dir)
+        # 关闭
         driver.close()
+        # 退出
         driver.quit()
+    # 合成全图
     def splice_image(self):
        
         for root, dirs, files in os.walk("./output",topdown=True):  
-            
             break
         for dir in dirs:
             total_width = 0
@@ -71,30 +78,34 @@ class Transfom():
             f_list = []
 
             for root, dirs, files in os.walk("./output/"+dir,topdown=True):
-                # print(files)
                 for file in files:
-                    with Image.open("./output/"+dir+"/"+file) as f:
-                        f_list.append(f)
-                        tup = f.size
-                        if tup[0] >total_width:
-                            total_width = tup[0]
-                        total_height += tup[1]
-                        # img.paste(img,(100,100),None)
-                        # img = Image.new('RGB',(total_width, total_height))
-                print(total_height,total_width)
-            blank_image = Image.new("RGB",(1885, total_height))
+                    try:
+                        with Image.open("./output/"+dir+"/"+file) as f:
+                            f_list.append(f)
+                            tup = f.size
+                            if tup[0] >total_width:
+                                total_width = tup[0]
+                            total_height += tup[1]
+                         
+                            # img.paste(img,(100,100),None)
+                            # img = Image.new('RGB',(total_width, total_height))
+                    except IOError:
+                        continue
+            blank_image = Image.new("RGB",(total_width, total_height))
             temp = 0
-            
+        
             for root, dirs, files in os.walk("./output/"+dir,topdown=True):
                 # print(files)
                 for file in files:
-                    with Image.open("./output/"+dir+"/"+file) as f:
-                        tup= f.size
-                        blank_image.paste(f,(0,temp+2))
-                        temp = tup[1]
-                    break
-                blank_image.save("data/"+file,quality = 95)
+                    try:
+                        with Image.open("./output/"+dir+"/"+file) as f:
+                            tup= f.size
+                            blank_image.paste(f,(0,temp+2))
+                            temp += tup[1]
+                    except IOError:
+                        continue
+                blank_image.save("data/"+dir+".png",quality = 95)
 
 if __name__ == "__main__":
-    # Transfom().splice_image()
     Transfom().main()
+    Transfom().splice_image()
--- util.py
+++ util.py
@@ -7,10 +7,10 @@ def fullpage_screenshot(driver, file, dir):
  
         print("Starting chrome full page screenshot workaround ...")
  
-        total_width = driver.execute_script("return document.body.offsetWidth")
-        total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
-        viewport_width = driver.execute_script("return document.body.clientWidth")
-        viewport_height = driver.execute_script("return window.innerHeight")
+        total_width = int(driver.execute_script("return document.body.offsetWidth"))
+        total_height = int(driver.execute_script("return document.body.parentNode.scrollHeight"))
+        viewport_width = int(driver.execute_script("return document.body.clientWidth"))
+        viewport_height = int(driver.execute_script("return window.innerHeight"))
         print("Total: ({0}, {1}), Viewport: ({2},{3})".format(total_width, total_height,viewport_width,viewport_height))
         rectangles = []
  
@@ -35,7 +35,7 @@ def fullpage_screenshot(driver, file, dir):
  
             i = i + viewport_height
  
-        stitched_image = Image.new('RGB', (total_width, total_height))
+        stitched_image = Image.new('RGB', (1600, total_height*2))
         previous = None
         part = 0
  
@@ -50,20 +50,23 @@ def fullpage_screenshot(driver, file, dir):
  
             driver.get_screenshot_as_file(file_name)
             screenshot = Image.open(file_name)
- 
+            
+            # screenshot = screenshot.resize((viewport_width*2, viewport_height*2),Image.ANTIALIAS)
+            
+            # 游标的长度
             if rectangle[1] + viewport_height > total_height:
-                offset = (rectangle[0], total_height - viewport_height)
+                offset = (rectangle[0], (total_height - viewport_height)*2)
             else:
-                offset = (rectangle[0], rectangle[1])
- 
+                offset = (rectangle[0], rectangle[1]*2)
+            
             print("Adding to stitched image with offset ({0}, {1})".format(offset[0],offset[1]))
             stitched_image.paste(screenshot, offset)
- 
+            
             del screenshot
             os.remove(file_name)
             part = part + 1
             previous = rectangle
  
-        stitched_image.save("output/"+dir+"/"+file,quality = 95)
+        stitched_image.save("output/"+dir+"/"+file,quality = 10)
         print("Finishing chrome full page screenshot workaround...")
         return True
